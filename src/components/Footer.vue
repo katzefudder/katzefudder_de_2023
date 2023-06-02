4209d07a85583128915a580bdaf31b42c286126a
Florian Dehn <flo@katzefudder.de>
2023-06-02 10:55:31 +0200

    added aws details

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div id="footer">
     <div class="container">
       <p>Made in Hessen =^.^= {{ new Date().getFullYear() }}, Florian Dehn, Feldbornstrasse 2, 35510 Butzbach</p>
-      <p>No Cookies, no server-side processing, or analyzed nor stored logs - just a plain VueJS App</p>
+      <p>No Cookies, no server-side processing, or analyzed nor stored logs - just a plain VueJS App powered by AWS S3 and CloudFront</p>
     </div>
   </div>
 </template>
