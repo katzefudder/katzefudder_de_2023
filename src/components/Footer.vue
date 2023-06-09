dfc164337eb3557b11f9a0bc0b00cf83d98c5024
Florian Dehn <flo@katzefudder.de>
2023-06-09 10:35:11 +0200

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="footer">
     <div class="container">
-      <p>Made in Hessen =^.^= {{ new Date().getFullYear() }}, Florian Dehn, Feldbornstrasse 2, 35510 Butzbach</p>
-      <p>No Cookies, no server-side processing, or analyzed nor stored logs - just a plain VueJS App powered by AWS S3 and CloudFront</p>
+      <p>No Cookies, no server-side processing, no analyzed or stored logs - just a plain VueJS App powered by AWS S3 and CloudFront</p>
+      <p>I use my homegrown <a href="https://vuejs.org" target="_blank">VueJS</a> <a href="https://github.com/katzefudder/FlickrGallery" target="_blank">Gallery</a> to display some of my photos on <a href="https://www.flickr.com/photos/katzenfutter/" target="_blank">Flickr</a> </p>
+      <h6>Made in Hessen =^.^= {{ new Date().getFullYear() }}, Florian Dehn, Feldbornstrasse 2, 35510 Butzbach</h6>
     </div>
   </div>
 </template>
