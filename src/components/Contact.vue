22364a4bfd83fa9599a55456ec65375d7d97c590
Florian Dehn <flo@katzefudder.de>
2023-07-17 10:25:16 +0200

    added https://www.wetterauer-zeitung.de/sport/ec-bad-nauheim/sommer-serie-vii-sie-sind-von-anfang-an-dabei-92399586.html

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -50,6 +50,7 @@
                 <li>
                   Other sources
                   <ul>
+                    <li><a href="https://www.wetterauer-zeitung.de/sport/ec-bad-nauheim/sommer-serie-vii-sie-sind-von-anfang-an-dabei-92399586.html" target="_blank">Sommer-Serie (VII): Sie sind von Anfang an dabei</a></li>
                     <li><a href="https://www.fnp.de/sport/lokalsport/verteidiger-marius-erk-man-kann-wieder-mit-uns-rechnen-92394804.html" target="_blank">Verteidiger Marius Erk: »Man kann wieder mit uns rechnen«</a></li>
                     <li><a href="https://www.wetterauer-zeitung.de/sport/ec-bad-nauheim/sommer-serie-iii-wie-marcel-brandt-die-fans-elektisieren-konnte-92329707.html" target="_blank">Wie Marcel Brandt die Fans elektisieren konnte</a></li>
                     <li><a href="https://www.wetterauer-zeitung.de/sport/ec-bad-nauheim/ec-bad-nauheim-pollastrone-bleibt-kooperation-verlaengert-92329095.html" target="_blank">EC Bad Nauheim: Pollastrone bleibt - Kooperation verlängert</a></li>
