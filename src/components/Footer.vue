7b8597bb020662d705ea0a6ad9c2e2da579c63c0
Florian Dehn <flo@katzefudder.de>
2024-05-15 16:59:20 +0200

    updated content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="footer">
     <div class="container">
-      <p>No Cookies, no server-side processing, no analyzed or stored logs - just a plain VueJS App powered by AWS S3 and CloudFront</p>
+      <p>VueJS App powered by AWS S3 and CloudFront</p>
       <p>I use my homegrown <a href="https://vuejs.org" target="_blank">VueJS</a> <a href="https://github.com/katzefudder/FlickrGallery" target="_blank">Gallery</a> to display some of my photos on <a href="https://www.flickr.com/photos/katzenfutter/" target="_blank">Flickr</a> </p>
       <h6>Made in Hessen =^.^= {{ new Date().getFullYear() }}, Florian Dehn, Feldbornstrasse 2, 35510 Butzbach</h6>
       <p>All trademarks, logos and brand names are the property of their respective owners.</p>
